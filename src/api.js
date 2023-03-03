42bad91e6abba62331b2367182adc5e7e4ec71e3
Spencer Taira <spencermtaira@gmail.com>
2023-03-03 15:42:11 -0800

    started intial foundations for joinRoom api call'

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const BASE_API_URL = "http://localhost:5000"; 
+const BASE_API_URL = "http://localhost:5000";
 
 class BoggleApi {
-   
+
    static async createRoom(roomData){
       //console.log("what is roomData", roomData);
       try{
@@ -30,17 +30,29 @@ class BoggleApi {
          return result.data;
       } catch (e) {
          console.log(e);
-         const err = new Error("Unable to join room");
+         const err = new Error("Unable to enter room");
          err.status = 400;
          throw err;
       }
    }
 
    static async joinRoom(playerData){
+      console.log("BOGGLE API JOIN ROOM playerDATA", playerData);
       try{
+         const result = await axios (
+            {
+               method: "post",
+               url: `${BASE_API_URL}/room/join`,
+               data: playerData
+            }
+         )
 
+         return result.data;
       } catch (e) {
-         
+         console.log(e);
+         const err = new Error("Unable to join room");
+         err.status = 400;
+         throw err;
       }
    }
 }
