c97535569861a28d6bffd7cce2b3ce1d313fb8c4
Spencer Taira <spencermtaira@gmail.com>
2023-03-30 15:07:35 -0700

    added a test functionality to help with debugging and clearing local and session storage

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -2,11 +2,21 @@ import React from "react";
 import { NavLink, Link } from "react-router-dom";
 
 function Nav() {
+
+    // Temp function clearing functionality to help with testing
+    function clearStorage() {
+        localStorage.clear();
+        sessionStorage.clear();
+    }
+
     return (
         <nav className="Nav">
            <Link to={"/"}>
                 Home
             </Link>
+            <button onClick={clearStorage}>
+                Clear
+            </button>
         </nav>
     )
 }
