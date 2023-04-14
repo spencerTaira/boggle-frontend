a37002f8e36b362f0e6947022c62fb876d89230f
Spencer Taira <spencermtaira@gmail.com>
2023-04-14 16:39:37 -0700

    Refactored useEffect to take up less space and remove some redundant websockets so it is more streamlined. We also restructured how we were rendering Lobby and Game components. We also added new state like joined, gameStart, players, and messages in order to keep track of our game state

--- src/components/lobby/Lobby.tsx
+++ src/components/lobby/Lobby.tsx
@@ -1,134 +1,136 @@
-import React, { useState, useEffect, useContext, useMemo } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import BoggleAPI from "../../api";
 import userContext from "../../userContext";
 import EnterPasswordForm from "./EnterPasswordForm";
-import ChatBox from "./ChatBox";
-import { socket, socketLobby } from "../../socket";
+import LobbyUI from "./lobbyUI/LobbyUI";
+import GameUI from "./gameUI/GameUI";
+import { socketLobby } from "../../socket";
+
+interface PlayerMessageInterface {
+    playerName: string,
+    message: string
+};
+
+interface PlayerInLobbyInterface {
+    playerName: string,
+    playerId: number
+}
 
 function Lobby() {
     console.debug('Entered Lobby');
 
+    const navigate = useNavigate();
     const { id } = useParams<{ id: string }>();
-    const { playerData, playerMessages, appendMessages } = useContext(userContext);
+    const { playerData } = useContext(userContext);
     const [lobbyData, setLobbyData] = useState(
         {
-            "curr_players": 0,
             "game_length": 0,
             "host": null,
             "lobby_name": "",
             "max_players": 0,
             "private": true
         }
-    )
-    
-    const [joined, setJoined] = useState(false)
-    const navigate = useNavigate();
+    );
+    const [players, setPlayers] = useState<Array<PlayerInLobbyInterface>>([]);
+    const [playerMessages, setPlayerMessages] = useState<Array<PlayerMessageInterface>>([]);
+    const [joined, setJoined] = useState(false);
+    const [gameStart, setGameStart] = useState(false);
 
     console.log("what is playerData in Lobby Component", playerData);
     console.log('What is lobby data?', lobbyData);
+    console.table(players);
+    console.table(playerMessages);
 
-    //TODO: Figure out where to add lobby verification with playerData in or out of useEffect???
+    function appendMessage(newMessage: PlayerMessageInterface) {
+        setPlayerMessages((prevMessages) => ([...prevMessages, newMessage]));
+    }
+
     useEffect(() => {
-        console.log('Lobby Use Effect Running');
+        console.debug('Lobby Use Effect Running');
         async function checkLobby() {
             const result = await BoggleAPI.checkLobby({ lobbyName: id });
 
             if (result.error) {
                 // able to send information along with navigate and access at the final destination
-                navigate(
-                    '/',
-                    {
-                        state: {
-                            error: result.error
-                        }
-                    }
-                )
+                navigate('/', { state: { error: result.error } });
             }
 
             if (result.lobbyData.curr_players >= result.lobbyData.max_players) {
-                navigate(
-                    '/',
-                    {
-                        state: {
-                            error: `Lobby ${id} full. Sorry :-(`
-                        }
-                    }
-                )
+                navigate('/', { state: { error: `Lobby ${id} full. Sorry :-(` } });
             }
 
-            setLobbyData(() => result.lobbyData)
+            setLobbyData(() => result.lobbyData);
         }
 
         checkLobby();
-        
+
         async function rejoinLobby(playerData:any){
             const result = await BoggleAPI.rejoinLobby(playerData)
             if (result.error) {
-                // able to send information along with navigate and access at the final destination
-                navigate(
-                    '/',
-                    {
-                        state: {
-                            error: result.error
-                        }
-                    }
-                )
+                navigate('/', { state: { error: result.error } });
             }
         }
 
-        if (playerData.currLobby === id && joined === false) {
-            rejoinLobby(playerData);
+        if (playerData.currLobby === id) {
+            if (lobbyData.host === playerData.playerId) {
+                setJoined(() => true);
+            } else if (!joined) {
+                rejoinLobby(playerData);
+            }
+
             socketLobby.emit("joining", playerData);
         }
 
-        if (playerData.currLobby === id && joined === true) {
-            socketLobby.emit("joining");
+        function updatePlayers(playersInLobby: Array<PlayerInLobbyInterface>) {
+            setPlayers(() => playersInLobby);
         }
 
-
-
-        function handleJoined(msg:Record<string, string>){
-            appendMessages(msg);
-            console.log("handleJoined msg", msg);
-        }
-
-        socketLobby.on('joined', handleJoined)
-        
         //We should never see our own leaving message
-        socketLobby.on('left', (msg)=>console.log(msg))
-        
+        socketLobby.on('message', appendMessage);
+        socketLobby.on('update_players', updatePlayers);
+
         return () => {
             socketLobby.emit('leave', playerData)
-            socketLobby.off('joined');
-            socketLobby.off('left');
+            socketLobby.off('message', appendMessage);
+            socketLobby.off('update_players', updatePlayers);
         };
-        
-        //Who is in lobby display (info)
-        //Websocket ask for other player data
-    }, [id, navigate, playerData]);
+    }, [id, navigate, playerData, joined]);
+
+    if (playerData.currLobby !== id) {
+        return (
+            <EnterPasswordForm id={id!} setJoined={setJoined} />
+        )
+    }
 
     return (
         <div>
-            {playerData.currLobby !== id
-                ?
-                <EnterPasswordForm id={id!} setJoined={setJoined} />
-                :
-                <div>
+            {/* {
+                !gameStart
+                ? <LobbyUI messages={playerMessages} players={} lobbyData={lobbyData} setGameStart={setGameStart} />
+                : <GameUI />
+            } */}
+                {/* <div>
                     <p>LOBBBBBY: {id}</p>
                     <p>I am player: {playerData.playerName}</p>
                     <ChatBox messagesData={playerMessages}/>
                 </div>
-            }
+            } */}
         </div>
     )
 }
 
 export default Lobby;
 
-//TODO: increment/decrement players in room (you can be here, but are you in the room?)
-//TODO: join room in database upon entering
 //TODO: Figure out when to update last active for player model
-//TODO: figure out what websockets will handle for data
 //TODO: change host if existing host leaves, or close room if no players
-//TODO: simple chat box to test specific room data
+
+/*
+
+                                                    Lobby
+                                                    state: messages, joined, gameStarted, players, lobbyInfo
+                                                    props:
+
+                                        Lobby Interface     Game
+                                        state: joined
+*/
