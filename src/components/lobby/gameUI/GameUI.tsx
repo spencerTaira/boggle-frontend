a2b459874e7b0c20eb2e84d6b85069ba432d39c0
Spencer Taira <spencermtaira@gmail.com>
2023-04-14 16:38:31 -0700

    restructuring our components so our Lobby component will render both our lobby interface and game inteface

---
+++ src/components/lobby/gameUI/GameUI.tsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect, useContext, useMemo } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import BoggleAPI from "../../../api";
+import userContext from "../../../userContext";
+import ChatBox from "../ChatBox";
+import { socket, socketLobby } from "../../../socket";
+
+function GameUI() {
+
+}
+
+export default GameUI;
---
+++ src/components/lobby/lobbyUI/LobbyUI.tsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect, useContext, useMemo } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import BoggleAPI from "../../../api";
+import userContext from "../../../userContext";
+import ChatBox from "../ChatBox";
+import { socket, socketLobby } from "../../../socket";
+
+/*
+    Renders LobbyUI component
+
+    State:
+
+
+    Props:
+        messages - [{
+            playerName: string,
+            message: string
+        }, ...]
+
+        players - [playerName (str), ...]
+
+        lobbyInfo - {
+            "game_length": 0,
+            "host": null,
+            "lobby_name": "",
+            "max_players": 0,
+            "private": true
+        }
+
+        startGame - callback
+*/
+function LobbyUI({messages, players, lobbyData, startGame}) {
+    console.debug('Lobby UI');
+
+    const { playerData } = useContext(userContext);
+
+}
+
+export default LobbyUI;
