9219ced6ec9d9625b42b9525b09fdb0b98c1a2fe
Spencer Taira <spencermtaira@gmail.com>
2023-05-03 14:09:43 -0700

    added in test console logs

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const URL = process.env.backendURL || 'http://localhost:8000';
-
+const URL = process.env.BACKENDURL || 'http://localhost:8000';
+console.log('BACKEND ENV VARIABLE', process.env.BACKENDURL, 'THIS HAS ENDED');
 export default URL;
