73a18e73ea71cacb3e9943177242376fcec5b76b
michael-bocim <mtbocim@gmail.com>
2023-08-24 18:36:16 -0600

    creating client id as query param for connect uses

--- src/socket.js
+++ src/socket.js
@@ -1,8 +1,14 @@
 import { io } from 'socket.io-client';
 import URL from './config';
+import { v4 as uuid } from 'uuid';
 
 //export const socket = io(URL, {autoConnect:false});
-export const socketIntro = io(`${URL}/intro`,{autoConnect:false});
+export const socketIntro = io(`${URL}/intro`,{
+   query: {
+     client_id: getPlayerId()
+   }
+ });
+// export const socketIntro = io(`${URL}/intro`,{autoConnect:false});
 export const socketLobby = io(`${URL}/lobby`, {
 //   reconnectionDelay: 10000, // defaults to 1000
 //   reconnectionDelayMax: 10000 // defaults to 5000
@@ -18,3 +24,16 @@ export const socketLobby = io(`${URL}/lobby`, {
 // socketLobby.on('reconnect', () => {
 //   console.log('Did we reconnect?', socketLobby);
 // })
+
+function getPlayerId(){
+   //check local storage
+   //if player id exists, return it
+   //otherwise, make it, store it, then return it
+
+   let clientId = localStorage.getItem('clientId');
+   if (!clientId) {
+      clientId = uuid()
+   }
+   return clientId
+   
+}
